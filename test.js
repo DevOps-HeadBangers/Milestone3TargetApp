0f84b87aa007b58de522c863754fd672e020b75f
kumar-utsav <kmr.utsav@gmail.com>
2015-10-22 19:40:28 -0400

    Modify project structure

---
+++ test.js
@@ -0,0 +1,17 @@
+var test = require('tape');
+var request = require('supertest');
+var app = require('./server');
+
+test('File upload test cases', function(t) {
+    request(app)
+        .post('/api/photo')
+        .field('filename', 'test file')
+        .attach('file', 'image.png')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+            // t.error(err, 'No error');
+            t.same(res.status, 200, 'should upload file');
+            t.end();
+        });
+});
