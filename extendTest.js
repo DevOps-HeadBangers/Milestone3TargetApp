c01a5c429bf85ac6a3a045121abac60527945a3e
kumar-utsav <kmr.utsav@gmail.com>
2015-10-23 02:18:38 -0400

    Extend analysis

--- extendTest.js
+++ extendTest.js
@@ -3,7 +3,7 @@ fs.readFile('test.tap', 'utf8', function(err, data) {
     // if (err) throw err;
     var index = data.indexOf("not ok");
     if(index != -1){
-    	console.log("Failed test cases present. Rejecting commit...");
+    	console.log("Failed test cases present. Failing the build...");
     	process.exit(1);
     }
 });
