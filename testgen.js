37e9aea65f5ab60e539456ce84c551dbef6c064a
kumar-utsav <kmr.utsav@gmail.com>
2015-10-22 20:42:18 -0400

    Modify test cases

--- testgen.js
+++ testgen.js
@@ -31,11 +31,15 @@ var content = '';
 
 function generateAPITestCases(funcName, params) {
 
-    for (var i = 0; i < params.length; i++) {
-        var param = params[i];
-        if (funcName === 'get') {
-            content = content + "\ntest('File upload test cases', function(t) { request(app).{0}('{1}').expect(200).end({2});});".format(funcName, param, callback);
-        }
+    // for (var i = 0; i < params.length; i++) {
+    //     var param = params[i];
+    //     if (funcName === 'get') {
+    //         content = content + "\ntest('File upload test cases', function(t) { request(app).{0}('{1}').expect(200).end({2});});".format(funcName, param, callback);
+    //     }
+    // }
+
+    if (funcName === 'get') {
+        content = content + "\ntest('File upload test cases', function(t) { request(app).{0}('{1}').expect(200).end({2});});".format(funcName, params, callback);
     }
 
     fs.appendFile('test.js', content, function(err) {
@@ -65,13 +69,15 @@ function constraints(filePath) {
 
                     var funcName = child.callee.property.name;
 
-                    var params = [];
+                    // var params = [];
 
-                    params.push(child.arguments[0].value);
-                    params.push('/auth/photos');
+                    // params.push(child.arguments[0].value);
+                    // params.push('/auth/photos');
+
+                    var params = child.arguments[0].value;
 
                     generateAPITestCases(funcName, params);
-                    
+
                 }
             });
         }
