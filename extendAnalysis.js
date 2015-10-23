3f1312fc6dbc2d161c92eece151656300f4c6655
kumar-utsav <kmr.utsav@gmail.com>
2015-10-23 02:20:07 -0400

    Update analysis extension

--- extendAnalysis.js
+++ extendAnalysis.js
@@ -8,7 +8,8 @@ fs.readFile(__dirname + '/checkstyle-result.xml', function(err, data) {
     parser.parseString(data, function (err, result) {
         var errorCount = result.checkstyle.file[0].error.length;
         if(errorCount > threshold){
-        	console.log("Lint error exceeded threshold. Failing the build...")
+        	console.log("Lint error exceeded threshold. Failing the build...");
+        	process.exit(1);
         }
     });
 });
