b3dc518189a2b66923424bef55f2527ee6016a51
kumar-utsav <kmr.utsav@gmail.com>
2015-11-18 11:38:30 -0500

    update redis

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ var app1 = http.createServer(function(req, res) {
     }),
     io = sio.listen(app1);
 
-var client = redis.createClient(6379, '104.131.193.51', {})
+var client = redis.createClient(6379, process.argv[2], {})
 
 client.set("M3_EMAIL", "Yes");
 
